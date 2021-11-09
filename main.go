f831ed2e887f724255e1c69594cf9e175dc32edc
Vinicius Robledo <vinicius.robledo@mercadolivre.com>
2021-11-09 10:02:39 -0300

    incluindo structs de person

--- main.go
+++ main.go
@@ -2,6 +2,40 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
 func main() {
-	fmt.Println("Hi there!")
+	vini := person{
+		firstName: "Vinicius",
+		lastName:  "Robledo",
+		contactInfo: contactInfo{
+			email:   "vini@gmail.com",
+			zipCode: 123456,
+		},
+	}
+
+	vini.print()
+
+	vini.updateName("Amanda")
+
+	vini.print()
+
+}
+
+func (p person) print() {
+	//Printf tem modificadores como o +v que exibe nome dos atributos também (normalmente só printa os valores dos atributos)
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
